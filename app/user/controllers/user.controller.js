4248c32dcc356390ecfcf5a187eea920f5c84b10
Budvin Chathura <chathura97rc@gmail.com>
2019-12-02 19:51:24 +0530

    json response

--- app/user/controllers/user.controller.js
+++ app/user/controllers/user.controller.js
@@ -11,7 +11,7 @@ module.exports.login = (req, res) => {
     const { error } = login_validation(req.body);
 
     if (error) {
-        return res.status(400).send(error.details[0].message);
+        return res.status(400).json({error:error.details[0].message});
     }
 
 
@@ -23,30 +23,30 @@ module.exports.login = (req, res) => {
                 if (password_match) {
                     // return res.status(200).send({ username: user.username });
                     const token = jwt.sign({username:user.username},process.env.TOKEN_SECRET);
-                    return res.status(200).header('erp-auth-token',token).send(token);
+                    return res.status(200).header('erp-auth-token',token).json();
                 } else {
-                    return res.status(400).send('invalid password');
+                    return res.status(400).json({error:'invalid password'});
                 }
 
             } else {
-                return res.status(400).send('could not find username');
+                return res.status(400).json({error:'could not find username'});
             }
         }).catch((err) => {
-            return res.status(500).send('server error');
+            return res.status(500).json({error:err});
         });
 }
 
 module.exports.register = (req, res) => {
     const { error } = register_validation(req.body);
     if (error) {
-        return res.status(400).send(error.details[0].message);
+        return res.status(400).json({error:error.details[0].message});
     }
 
     var user_model = new _user_model();
     user_model.find_by_username_or_employee_id(req.body.username, req.body.employee_id)
         .then(async (result) => {
             if (result) {
-                return res.status(400).send('duplicate entry');
+                return res.status(400).json({error:'duplicate entry'});
             } else {
                 const salt = await bcrypt.genSalt(10);
                 const hashed_password = await bcrypt.hash(req.body.password, salt);
@@ -56,14 +56,17 @@ module.exports.register = (req, res) => {
                 user_model.insert()
                 .then((result) => {
                     if (result) {
-                        res.status(200).send({ username: user_model.username });
+                        res.status(200).json({ username: user_model.username });
                     }
+                })
+                .catch((err)=>{
+                    return res.status(500).json({error:err});
                 })
             }
         })
         
         .catch((error) => {
-            return res.status(500).send(error);
+            return res.status(500).json({error:error});
         })
 
     // .catch((error)=>{
