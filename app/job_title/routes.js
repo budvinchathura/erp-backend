c92f2146eff4db165c8bc46b4c59f52203f6cb32
Budvin Chathura <chathura97rc@gmail.com>
2019-12-12 19:01:59 +0530

    route change (post -> get)

--- app/job_title/routes.js
+++ app/job_title/routes.js
@@ -3,6 +3,6 @@ const job_title_controller = require('./controllers/job_title.controller');
 const {valid_jwt_needed} = require('../common/middleware/validation');
 const {minimum_access_level_required} = require('../common/middleware/access_level');
 
-router.post('/view', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],job_title_controller.view);
+router.get('/view', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],job_title_controller.view);
 
 module.exports = router;
