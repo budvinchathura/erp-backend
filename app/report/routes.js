aacf09bd286c2d6eb55c6647e16fbcddfd6db76d
Thumula Perera <thumulaperera@gmail.com>
2019-12-12 13:58:21 +0530

    report routes

--- app/report/routes.js
+++ app/report/routes.js
@@ -3,6 +3,5 @@ const report_controller = require('./controllers/report.controller');
 const {valid_jwt_needed} = require('../common/middleware/validation');
 const {minimum_access_level_required} = require('../common/middleware/access_level');
 
-router.post('/view-emp-leave', report_controller.view_employee_leave);
 
 module.exports = router;
