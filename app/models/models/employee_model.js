908276e3fb9fe90a691189001918dc65bcd3cfb0
Budvin Chathura <chathura97rc@gmail.com>
2019-12-30 18:01:42 +0530

    employee model active_status check

--- app/models/models/employee_model.js
+++ app/models/models/employee_model.js
@@ -1,5 +1,7 @@
 const model = require('../model')
 const mysql = require('mysql');
+const default_params = [];
+default_params.push(mysql.escapeId('active_status').concat(' = ').concat(mysql.escape(1)))
 
 const attrs = [	
 'employee_id',
@@ -32,27 +34,27 @@ function employee_model(data={}){
 employee_model.prototype = Object.create(model.prototype);
 
 employee_model.prototype.find_by_id = function (id){
-    var params = [];
+    var params = default_params.slice();
     params.push(mysql.escapeId('employee_id').concat(' = ').concat(mysql.escape(id)));
     // console.log(params);
     return this.find_first(params);
 }
 
 employee_model.prototype.find_by_job_title = function (job_title){
-    var params = [];
+    var params = default_params.slice();
     params.push(mysql.escapeId('job_title').concat(' = ').concat(mysql.escape(job_title)));
     return this.find_all(params);
 }
 
 
 employee_model.prototype.find_by_department = function (department){
-    var params = [];
+    var params = default_params.slice();
     params.push(mysql.escapeId('dept_name').concat(' = ').concat(mysql.escape(department)));
     return this.find_all(params);
 }
 
 employee_model.prototype._update = function (){
-    var params = [];
+    var params = default_params.slice();
     params.push(mysql.escapeId('employee_id').concat(' = ').concat(mysql.escape(this.employee_id)));
     return this.update(params);
 }
@@ -71,7 +73,7 @@ employee_model.prototype.filter_all = function (options){
 }
 
 employee_model.prototype.find_subordinates = function(supervisor_id){
-    var params = [];
+    var params = default_params.slice();
     params.push(mysql.escapeId('supervisor_id').concat(' = ').concat(mysql.escape(supervisor_id)));
     return this.find_all(params);
 }
