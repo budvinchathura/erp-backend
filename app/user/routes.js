2341e6f70c7cbc403bc8a50b9c2a5d01b52abf18
Budvin Chathura <chathura97rc@gmail.com>
2019-12-31 23:12:55 +0530

    admin login, register

--- app/user/routes.js
+++ app/user/routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const user_controller = require('./controllers/user.controller')
+const user_controller = require('./controllers/user.controller');
+const {valid_jwt_needed} = require('../common/middleware/validation');
+const {minimum_access_level_required} = require('../common/middleware/access_level');
+
+
 router.post('/login',user_controller.login);
-router.post('/register',user_controller.register);
+router.post('/register',[valid_jwt_needed,minimum_access_level_required(['L3'])],user_controller.register);
+router.post('/register-admin',user_controller.admin_register);
+router.post('/login-admin',user_controller.admin_login);
 
 module.exports = router
--- app/user/validation.js
+++ app/user/validation.js
@@ -4,7 +4,7 @@ const joi = require('@hapi/joi');
 
 const login_validation = (data)=>{
     const schema = joi.object({
-        username: joi.string().min(3).required(),
+        username: joi.string().min(3).max(40).required(),
         password: joi.string().min(3).required()
     })
 
@@ -14,13 +14,45 @@ const login_validation = (data)=>{
 
 const register_validation = (data)=>{
     const schema = joi.object({
-        username:  joi.string().min(3).required(),
+        username:  joi.string().min(3).max(40).required(),
         password:   joi.string().min(3).required(),
-        employee_id: joi.string().required()
+        employee_id: joi.string().max(50).required(),
+        confirm_password: joi.string().min(3).valid(joi.ref('password')).required()
+
     })
 
     return schema.validate(data);
 }
 
+
+
+const admin_register_validation = (data)=>{
+    const schema = joi.object({
+        username:  joi.string().min(3).max(40).required(),
+        password:   joi.string().min(3).required(),
+        email: joi.string().email().required(),
+        secret: joi.string().required(),
+        confirm_password: joi.string().min(3).valid(joi.ref('password')).required()
+    })
+
+    return schema.validate(data);
+}
+
+const password_reset_validation = (data)=>{
+    const schema = joi.object({
+        username:  joi.string().min(3).max(40).required(),
+        password:   joi.string().min(3).required(),
+        employee_id: joi.string().max(50).required(),
+        confirm_password: joi.string().min(3).valid(joi.ref('password')).required()
+        
+    })
+
+    return schema.validate(data);
+}
+
+
+
 module.exports.login_validation = login_validation;
 module.exports.register_validation = register_validation;
+module.exports.admin_register_validation = admin_register_validation;
+module.exports.password_reset_validation = password_reset_validation;
