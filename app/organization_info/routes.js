bcf1b93d91db50e38693b859c57b533bbd81cec2
Thumula <thumusteam@gmail.com>
2019-12-31 23:08:45 +0530

    organization info routes access level

--- app/organization_info/routes.js
+++ app/organization_info/routes.js
@@ -4,9 +4,9 @@ const {valid_jwt_needed} = require('../common/middleware/validation');
 const {minimum_access_level_required} = require('../common/middleware/access_level');
 
 router.get('/', organization_info_controller.view);
-router.post('/', [valid_jwt_needed,minimum_access_level_required(['L3'])], organization_info_controller.insert);
-router.post('/bulk-insert', [valid_jwt_needed,minimum_access_level_required(['L3'])], organization_info_controller.bulk_insert);
-router.delete('/', [valid_jwt_needed,minimum_access_level_required(['L3'])], organization_info_controller.delete);
-router.patch('/', [valid_jwt_needed,minimum_access_level_required(['L3'])], organization_info_controller.update);
+router.post('/', [valid_jwt_needed,minimum_access_level_required(['L3','Admin'])], organization_info_controller.insert);
+router.post('/bulk-insert', [valid_jwt_needed,minimum_access_level_required(['L3','Admin'])], organization_info_controller.bulk_insert);
+router.delete('/', [valid_jwt_needed,minimum_access_level_required(['L3','Admin'])], organization_info_controller.delete);
+router.patch('/', [valid_jwt_needed,minimum_access_level_required(['L3','Admin'])], organization_info_controller.update);
 
 module.exports = router;
