438f155a9c92d34b755974b806574cc701ca23f8
Thumula <thumusteam@gmail.com>
2020-01-01 08:35:45 +0530

    employment status update

--- app/models/models/employment_status_model.js
+++ app/models/models/employment_status_model.js
@@ -22,6 +22,11 @@ employment_status_model.prototype._find_all = function(){
     return this.find_all([]);
 }
 
+employment_status_model.prototype._update = function (old_employment_status){
+    var params = [];
+    params.push(mysql.escapeId('employment_status').concat(' = ').concat(mysql.escape(old_employment_status)));
+    return this.update(params);
+}
 
 
 module.exports = employment_status_model;
