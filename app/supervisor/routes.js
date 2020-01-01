6ec06a48314dcb5f837bd82a66507e468f75e53a
Thumula <thumusteam@gmail.com>
2020-01-01 09:41:34 +0530

    supervisor sub-laves route from get to post

--- app/supervisor/routes.js
+++ app/supervisor/routes.js
@@ -4,7 +4,7 @@ const {valid_jwt_needed} = require('../common/middleware/validation');
 const {minimum_access_level_required} = require('../common/middleware/access_level');
 
 router.get('/subs', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],supervisor_controller.subordinates);
-router.get('/sub-leaves', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],supervisor_controller.subordinate_leaves);
+router.post('/sub-leaves', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],supervisor_controller.subordinate_leaves);
 router.post('/sub-leave', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],supervisor_controller.change_leave_state);
 
 module.exports = router;
