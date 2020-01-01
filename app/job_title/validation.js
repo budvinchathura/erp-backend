a86933d1894b9130a9dc01a535e900732a773a35
Thumula <thumusteam@gmail.com>
2020-01-01 08:13:53 +0530

    update leave type

--- app/job_title/validation.js
+++ app/job_title/validation.js
@@ -1,8 +1,10 @@
 const joi = require('@hapi/joi');
 
+const job_title_size = 50;
+
 const job_title_add_update_validation = (data)=>{
     const schema = joi.object({
-        job_title: joi.string().required(),
+        job_title: joi.string().max(job_title_size).required(),
         access_level:joi.string().required().valid('L1','L2','L3','Admin')
     })
 
@@ -13,11 +15,11 @@ const job_title_add_update_validation = (data)=>{
 module.exports.job_title_update_validation = (data)=>{
     const schema = joi.object({
         old:joi.object({
-            job_title: joi.string().required(),
+            job_title: joi.string().max(job_title_size).required(),
             access_level:joi.string().required().valid('L1','L2','L3','Admin')
         }).required(),
         new:joi.object({
-            job_title: joi.string().required(),
+            job_title: joi.string().max(job_title_size).required(),
             access_level:joi.string().required().valid('L1','L2','L3','Admin')
         }).required(),
     })
--- app/leave/routes.js
+++ app/leave/routes.js
@@ -4,13 +4,14 @@ const {valid_jwt_needed} = require('../common/middleware/validation');
 const {minimum_access_level_required} = require('../common/middleware/access_level');
 
 router.get('/limits', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],leave_controller.view_limits);
-router.patch('/limit', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],leave_controller.update_limit);
-router.put('/limit', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])],leave_controller.add_limit);
+router.patch('/limit', [valid_jwt_needed,minimum_access_level_required(['L3'])],leave_controller.update_limit);
+router.put('/limit', [valid_jwt_needed,minimum_access_level_required(['L3'])],leave_controller.add_limit);
 router.get('/taken', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])], leave_controller.taken);
 router.post('/apply', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])], leave_controller.apply_leave);
 router.get('/my-types', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])], leave_controller.my_leave_types);
 router.get('/history', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])], leave_controller.history);
 router.get('/type', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])], leave_controller.view_leave_types);
-router.put('/type', [valid_jwt_needed,minimum_access_level_required(['L1','L2','L3'])], leave_controller.add_leave_type);
+router.put('/type', [valid_jwt_needed,minimum_access_level_required(['L3'])], leave_controller.add_leave_type);
+router.patch('/type', [valid_jwt_needed,minimum_access_level_required(['L3'])], leave_controller.update_leave_type);
 
 module.exports = router;
